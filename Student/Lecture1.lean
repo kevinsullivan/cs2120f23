d5dd00032e3d76655f25adc0ac3d7abe4c1a8864
rahilkarkar <rahilkarkar1@gmail.com>
2023-08-31 19:46:25 +0000

    Main

---
+++ Student/Lecture1.lean
@@ -0,0 +1,12 @@
+def id_nat : Nat → Nat
+  | n => n
+
+def id_bool : Bool → Bool
+  | true => true
+  | false => false
+
+def id_bool : Bool → Bool
+  | b => b
+
+def id_string : String → String
+  | n => n
