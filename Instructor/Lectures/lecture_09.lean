0904b495eaeb076d1bd326786921bddc4ede41ef
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-09-19 19:14:32 +0000

    Updates for class.

--- Instructor/Lectures/lecture_09.lean
+++ Instructor/Lectures/lecture_09.lean
@@ -194,10 +194,18 @@ you will be able to implement such a function?
 /-!
 ## Exercises
 
-- Define a function, nxe2s : Nat × Empty → String 
+- Can you define some function, nxe2s : Nat × Empty → String 
+- Is the type, Nat × Empty → String, inhabited or not?
 - How many strings can nxe2s possibly return? Why?
-- Define a function, noe2s : Nat ⊕ Empty → String 
-- How many strings can noe2s possibly return? Why?
+- Can you define a function, noe2s : String ⊕ Empty → Nat 
+- Is the type, String ⊕ Empty → Nat, inhabited or not?
+- Can noe2s return any Nat? If so, prove it by example.
+- Is the function type, (Nat → Empty), inhabited or not?
+- Prove your answer (is (Nat → Empty) uninhabited)
+- Is the type, {α : Type} → α → Empty, inhabited or not.
+- Prove your answer (Is {α : Type} → α → Empty, inhabited?)
+- Is the type, Empty → (Nat → Empty) inhabited? Prove it.
+- Prove this type uninhabited: {α : Type} → α × (α → Empty)
 -/
 
 end cs2120
--- Instructor/SUMMARY.md
+++ Instructor/SUMMARY.md
@@ -8,6 +8,7 @@
 - [Lecture 07](./Lectures/lecture_07.lean.md)
 - [Lecture 08](./Lectures/lecture_08.lean.md)
 - [Lecture 09](./Lectures/lecture_09.lean.md)
+- [Lecture 10](./Lectures/lecture_10.lean.md)
 - [Homework #2](./Homework/hw2.lean.md)
 - [Homework #3](./Homework/hw3.lean.md)
 - [Homework #4](./Homework/hw4.lean.md)
