e8baf4fb005e59f17f39d371805d2149b87370e0
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-09-23 15:17:28 +0000

    .

--- Instructor/Lectures/lecture_10.lean
+++ Instructor/Lectures/lecture_10.lean
@@ -304,13 +304,13 @@ def is_zero'' : Nat → Bool  -- this works but is verbose
 | Nat.zero => true
 | (Nat.succ n') => false 
 
-def is_zero' : Nat → Bool   -- (1 + n') is not a pattern
+def is_zero' : Nat → Bool   
 | 0 => true
-| (1 + n') => false           
+| (1 + n') => false         -- (1 + n') not a valid pattern     
 
 def is_Zero' : Nat → Bool   -- our preferred notation
 | 0 => true                 -- 0 for Nat.zero
-| n' + 1 => false           -- n' + 1 for (Nat.succ n')      
+| n' + 1 => false           -- n' + 1 is a valid pattern    
 
 /-!
 ### Exercises
@@ -324,7 +324,7 @@ Hint: Look at how you wrote *inner* for the *Doll* type.
 
 def pred : Nat → Nat 
 | 0 => 0
-| (Nat.succ n') => n'
+| (Nat.succ n') => n'   -- destructures nat>0 to (succ n')
 
 -- Answer here
 
@@ -341,9 +341,9 @@ returns the same doll as *d3*.
 
 -- Answer here
 
-def mk_doll : Nat → Doll
-| 0 => solid
-| n' + 1 => shell (mk_doll n')
+def mk_doll : Nat → Doll 
+| _ => _
+| _ => _
 
 -- test cases
 #check mk_doll 3
@@ -360,12 +360,12 @@ def nat_eq : Nat → Nat → Bool
 | 0, 0 => true
 | 0, n' + 1 => false
 | n' + 1, 0 => false
-| (n' + 1), (m' + 1) => nat_eq n' m'
+| (n' + 1), (m' + 1) => nat_eq _ _
 
 #eval nat_eq 6 5
 
 /-!
-#3: Write a function, *nat_eq : Nat → Nat → Bool*, that
+#3: Write a function, *nat_le : Nat → Nat → Bool*, that
 takes any two natural numbers and that returns Boolean 
 *true* if the first value is less than or equal to the 
 second, and false otherwise. Hint: The key to solving
@@ -374,6 +374,18 @@ on them, and then return the right result *in each case*.
 -/
 
 -- Here
+
+def nat_le : Nat → Nat → Bool
+| 0, _ => true
+| (n' + 1), 0 => false
+| (_), (_) => _
+
+#eval nat_le 0 0    -- expect true
+#eval nat_le 0 2    -- exect true
+#eval nat_le 1 0    -- expect false
+#eval nat_le 1 1    -- expect true
+#eval nat_le 2 1    -- expect false
+#eval nat_le 2 3    -- expect false
 
 /-!
 
@@ -395,74 +407,3 @@ is structurally smaller than that argument to nat_add.
 def add : Nat → Nat → Nat
 | m, 0 => m
 | m, (Nat.succ n') => _
-
-/-!
-## The List α Data Type
-
-A small extension to the types we've defined so far 
-will let us represent mathematical *lists* of objects
-as terms of an inductive type. Given any type of list
-elements, α, we can define a list of elements of that
-type as either the empty list (which we'll call *nil*)
-of as being constructed from two parts: an object of
-type α (we'll call it the *head* of the list) and a
-one-smaller *list* of objects of the same type. 
--/
-
-/-!
-### Constructors
--/
-
-namespace cs2120
-
-inductive List (α : Type) : Type 
-| nil
-| cons (head : α) (tail : List α) 
-
-open List
-
-end cs2120
-
-def l0 := @List.nil Nat   -- [] : List Nat
-def l1 := List.cons 0 l0  -- [0, []]
-def l2 := List.cons 1 l1  -- [1, [0]]
-def l3 := List.cons 2 l2  -- [2, [1, [0, []]]]
-
-/-!
-Lean provides convenient notations.
-
-- [] is notation for the empty list
-- :: is infix notation for List.cons
-- [a1, a2, a3, ...] is for a1::a2::a3::...::nil
--/
-
-#eval ([] : List Nat)
-#eval 0::[]
-#eval 3::2::1::0::[]
-#eval [3,2,1,0]
-
-/-!
-
-### Elimination
-
-Elimination is just like with the Doll and Nat 
-types, by pattern matching, but now you have to
-account for the additional element of each non-nil
-term of type List α.
--/
-
-def len {α : Type} : List α → Nat
-| [] => 0
-| _::t => 1 + len t
-
-#reduce len []
-#reduce len [3,2,1,0]
-#reduce len (3::2::1::0::[] : List Nat) 
-
-/-!
-Exercise. Write a function polymorphic in one type,
-α, that takes two arguments of type List α and returns
-the result of appending the second list after the first.
-Hint: try case analysis on the first list argument.   
--/
-
