e705be9c816ddf2c894a7de1449692e7c41fdd29
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-10-03 19:59:12 +0000

    Updates to lecture 13. Not done yet.

--- Instructor/Lectures/lecture_13.lean
+++ Instructor/Lectures/lecture_13.lean
@@ -4,7 +4,7 @@ UNDER CONSTRUCTION. THIS MATERIAL WILL EVOLVE.
 
 We repeat (and would ideally import) our specification of
 propositional logic, to set up for the new material in this
-chapter. We then present a function that when given the number,
+chapter. We then present a function that, when given the number,
 n, of variables (columns) in a truth table, returns a list of 
 all 2^n interpretation functions. By evaluating a given logical
 expression under each such interpretation we'll get the list of
@@ -21,40 +21,31 @@ logic, without commentary or section structure.
 -/
 
 structure var : Type :=  (n: Nat)
-
 inductive unary_op : Type | not
-
 inductive binary_op : Type
 | and
 | or
 | imp
-
 inductive Expr : Type
 | var_exp (v : var)
 | un_exp (op : unary_op) (e : Expr)
 | bin_exp (op : binary_op) (e1 e2 : Expr)
-
 notation "{"v"}" => Expr.var_exp v
 prefix:max "¬" => Expr.un_exp unary_op.not 
 infixr:35 " ∧ " => Expr.bin_exp binary_op.and  
 infixr:30 " ∨ " => Expr.bin_exp binary_op.or 
 infixr:25 " ⇒ " =>  Expr.bin_exp binary_op.imp
 infixr:20 " ⇔ " => Expr.bin_exp binary_op.iff 
-
 def eval_un_op : unary_op → (Bool → Bool)
 | unary_op.not => not
-
 def implies : Bool → Bool → Bool
 | true, false => false
 | _, _ => true
-
 def eval_bin_op : binary_op → (Bool → Bool → Bool)
 | binary_op.and => and
 | binary_op.or => or
 | binary_op.imp => implies
-
 def Interp := var → Bool  
-
 def eval_expr : Expr → Interp → Bool 
 | (Expr.var_exp v),        i => i v
 | (Expr.un_exp op e),      i => (eval_un_op op) (eval_expr e i)
@@ -69,7 +60,7 @@ a natural number, n, specifying a number of propositional
 variables, and that returns a list of 2^n interpretations 
 for the variables, *mk_var 0 ... mk_var n-1*. 
 
-#### Key Insight
+#### Key Observations
 
 To make the idea clearer, what mk_interps does is to take
 a number of variables, which you can think of as labels on
@@ -91,11 +82,11 @@ of a truth table with three propositional variables.
 |  T   |  T   |  F   |
 |  T   |  T   |  T   |
 
-There's a great deal of structure here. What do we find if
-we add a column on the left with the natural number index of
-each row, ranging from 0 to 7, while replacing the true (T)
-and false (F) values in the table with the binary digits, 1 
-for true and 0 for false?
+There's a great deal of structure here. Let's see what we
+get if we (2) add a column on the left with the natural 
+number *index* of each of the eight rows, ranging from 0 
+to 7, and (2) replace the true (T) and false (F) values 
+in the table with the binary digits (*bits*), 1 and 0.
 
 
 | row  |  v₀  |  v₁  |  v₃  |
@@ -112,84 +103,70 @@ for true and 0 for false?
 Do you see it? What's the relationship between
 the row index and the sequence of binary digits
 in each row? Think about it before reading on.
+The answer: each row of binary digits represents
+the corresponding row index in binary notation.
 
-The answer, of course, is that each sequence of
-binary digits is simply the binary representation
-of the row index. 
+More generally, each row of a truth table with 
+*v* variables (columns) represents one of the 
+*2^v* possible combinations of *v* bit values, 
+which is to say, a natural numbers from *0* to
+*2^v-1*. The upshot is that we can compute the
+*r'th* row of the input side of a truth table
+by doing nothing more than representing the row
+index as a binary numeral, padded with zeros on
+the left so that it's at least *v* digits wide. 
 
-#### Algorithm Design Strategy
+Furthermore, the association of the variables in
+the columns with the bits in any row represents
+one of the *2^v* possible *interpretations* of
+the variables in some expression of interest. 
 
-This insight unlocks an algorithm 
-design strategy. For each row index, compute the
-corresponding list of binary digits (which we can
-represent at Nats), add any zero padding on the 
-left to each row to equal the number of columns,
-convert these lists of binary digits (Nat) to Bool
-values, derive a function (interpretation) taking 
-each variable to the corresonding Bool (defaulting
-to, say, false, for all variable beyond the few we
-care about), and finally return the whole list of 
-2^n interpretation functions. To compute the output
-column of the truth table we can iteratively use
-each function in this list as the Interp argument
-to our expression evaluation (semantic evaluation)
-function.    
+#### Algorithm Design
 
+These insights unlock an algorithm design strategy. 
+First, define a function that, when given a number
+of variables, *v*, and a row index, *r*, returns the
+interpretation that associates the variables in the 
+columns to the Boolean represented by the bit values 
+in the binary representation of *r*. 
 
-We'll take a bottom-up approach to implementation
-by building and teting the required functions and
-then combining them into the desired overall result.
-
-- convert a row index into a list of binary digits
-- convert a list of binary digits into a list of Bools
-- convert a list of Bools into a function from corresponding variables
-- convert number, n, of variables to number, 2^n, of rows 
-- generate a list of such functions, per row index from 2^n-1 down to 0
+The top-level algorithm then simply takes a number 
+of variables, *v*, and returns a list of the *2^c* 
+interpretations for row indices from 0 to *2^v-1.*
 -/
-
 
 /-!
 #### Row Index to List of Binary Digits
 
-The rightmost digit of a binary expansion of a number, n,
-is 0 if n is even and 1 if n is odd. In other words, the
-rightmost binary digit is n%2. To get the next digit, you
-use integer division to divide n by 2, and repeat. 
+We'll start by defining a function that converts a 
+given natural number into its binary representation 
+as a *list* of binary digits. We'll just the natural
+numbers, *0* and *1* to represent binary digits.
 
-Here's a first version. If the input is 0 it returns the
-list of binary digits, [0]. Otherwise it returns a list 
-of all the digits to the left of the last digit, whcih is
-computed recursively, with the singlton list containing
-the correct rightmost digit appended at the end.
+The key observations here are (1) the rightmost bit
+of a binary representation of a number is *0* if the
+number is even and *1* if it's odd; (2) the rest of
+the bits are shifted one place to the right, dropping
+the rightmost bit, by division by 2. So what we do,
+while bits remain is to repeatedly (1) compute the 
+rightmost bit, (2) shift the rest of the bits right.
 
-As an example, suppose we're given a row index of 5.
-We want its binary expansion as a list of bits, which 
-is [1,0,1]. That's *1*2^2 + 0*2^1 + 1*2^0*, which in
-turn is *4 + 0 * 1*, which is 5. 
+To illustrate, consider row number, *5*. The binary 
+representation of *5* is *101*. From *right* to *left*
+that's 1*2^0 + 0*2^1 + 1*2^2 = 1 + 0 + 4 = 5.* The 
+number is odd so the rightmost bit is *1*, which is 
+the remainder, 5 % 2 = 1*. Now recurse on *5* shifted
+right by one bit, i.e., on *5/2* using natural number 
+division. *5/2* is *2*, so the nexxt bit is *2 % 2 = 0.* 
+Recursing on *2 / 2 = 1,* we find the next bit to be
+*1 % 2 = 1.* Finally recurse on *1 / 2 = 0*. Zero is
+one of the two base cases, so we're done. The result 
+is the sequence of bits *[1, 0, 1]*. Exercise: Do it 
+yourself for *6*. What's the second base case?   
 
-The basic idea then is as follows. If the input, n, is 
-0, we'll return [0] (the list containing just zero). If
-the the input is 1, we'll return [1]. Otherwise n is at
-least 2, so we can write it as n = n' + 2. We thus have
-three cases to consider, and have already give solutions
-for the first two.
-
-So now suppose *n = n' + 2* for some *n'*. The rightmost 
-bit is (n%2)*. Once we have this bit in hand, we want to 
-eliminate it, shift all the remaining bits one place to 
-the right and repeat. To throw away the rightmost bit of 
-*n*, we just divide it by 2.
-
-So now we have our algorithm. If n is a base case, output 
-the corresponding singleton list of binary digits. In any
-other case, we output the list obtained by appending two
-lists: (1) the recursively computed list of digits to the
-left of the rightmost bit, and (2) the list containing just
-the rightmost bit. 
-
-To make our code even clearer, we'll *reify* the abstract
-operations of extracting the rightmost bit, and shifting
-all remaining bits to the right.
+Here's code. We abstract the computations of the right
+bit and shift right operations as named functions. The
+recursion, sadly, is not structural. 
 -/
 
 def right_bit (n : Nat) := n%2
@@ -204,43 +181,66 @@ def nat_to_bin : Nat → List Nat
   nat_to_bin (shift_right (n' + 2)) ++ [right_bit (n' + 2)]
 
 /-!
-Now you're probably wondering, what's that mysterious
+Okay, you're wondering, what's that weird expression,
 *have : (shift_right (n' + 2)) < (n' + 2) := sorry?*
-To make a long story short, the recursion here is 
-*not* structural. (Why?) That means that Lean won't
-be able to see that the argument to the recursion is
-always decreasing. To avoid Lean giving an error saying 
-it can't prove termination (because it can't tell that 
-the argument is decreasing), we have to give Lean an
-explicit proof of it. The mystery code tells Lean that 
-we do have such a proof, while the *sorry* says, *but 
-we're not going to give it now, just trust us.* That's 
-good enough for Lean not to complain. We'll come back 
-to termination at a later point, under the guise of 
-*well founded relations*. 
+To make a long story short, the recursion here is not 
+structural. (Why?) That means that Lean won't be able 
+to prove to itself that the argument to the recursion is
+always decreasing, which is needed to prove that the
+recursion always terminates. To avoid Lean giving an 
+error, we have to give Lean an explicit proof that the
+argument decreases on each recursive call. The mystery 
+code tells Lean that we have such a proof. The *sorry*
+says, *but we're not going to give it now; just trust 
+us.* That's good enough for Lean not to complain. For
+now, that's what you need to know. 
 -/
+
+#eval nat_to_bin 0  -- expect [0]
+#eval nat_to_bin 1  -- expect [1]
+#eval nat_to_bin 3  -- expect [1,1]
+#eval nat_to_bin 5  -- expect [1,0,1]
+#eval nat_to_bin 6  -- expect [1,1,0]
 
 /-!
-As a next processing step, we need to fill each
-resulting list of bits with zeros on the left to
-make each list equal in length to the number of
-variables being considered in a given situation.
-For example, the list of bits returned when *n=1*
-is just *[1]* but a truth table with three variable
-columns will have *[0,0,1]* in the row with index 1.
-We'll iteratively prepend zeros to a given list a
-number of times equal to the number of variables
-minus the list length. In Lean *v - l* is zero in
-all cases where *l ≥ v*, so our function will do
-nothing if the input list is already long enough. 
+#### Left Padding with Zeros
+
+We represent *F* (false) values in our truth tables
+as zeros. One remaining issue is that we have to make
+each output list of binary digits *v* digits long, to
+include *F* values (zeros) on the left. That is, we
+need to left-pad our lists with zeros so that each 
+list is at least *v* digits wide. 
+
+To do so, we'll iteratively prepend zeros to a given 
+list a number of times equal to *v* minus the length
+of a given list. In Lean *v - l* is zero in all cases 
+where *l ≥ v* (these are natural numbers), so there is
+nothing left to do if the input list is long enough. 
+
+It's a pretty easy recursion. We take this opportunity
+to show how you can do a little top-down programming
+by writing a top-level program that uses one that is
+then provided as a sort of private subroutine. Learn
+how to write code like this.
+
+Finally, we note that it's very common to implement a
+function as a non-recursive top-level program that in
+turn calls a recursive subroutine, as illustrated here.
 -/
 
--- Note new Lean construct introduced here
 def zero_pad : Nat → List Nat → List Nat
-  | v, l => zero_pad_helper (v - (l.length)) l
-where zero_pad_helper : Nat → List Nat → List Nat
+  | v, l => zero_pad_recursive (v - (l.length)) l
+where zero_pad_recursive : Nat → List Nat → List Nat
   | 0, l => l
-  | v'+1, l => zero_pad_helper v' (0::l)
+  | v'+1, l => zero_pad_recursive v' (0::l)
+
+#eval zero_pad 3 [0]
+#eval zero_pad 3 [1]
+#eval zero_pad 3 [1,1]
+#eval zero_pad 3 [0,1,1]
+#eval zero_pad 3 [1,0,1]
+#eval zero_pad 5 [1,0,1]
 
 /-!
 We can now write a function that will produce the
@@ -252,6 +252,8 @@ of the) n'th row of a truth table with v variables
 def mk_bit_row : (row: Nat) → (cols : Nat) → List Nat
 | r, c => zero_pad c (nat_to_bin r)
 
+#eval mk_bit_row 5 6  -- expect [0, 0, 0, 1, 0, 1]
+
 /-!
 Next we need a function to convert a list of bits
 (Nats) to a list of corresponding Bools. We will 
@@ -259,7 +261,7 @@ convert Nat zero to false, and any other Nat to
 true.
 -/
 
--- Convert individual nat to bool
+-- Convert nat to bool where 0 ↦ false, ¬0 ↦ true
 def bit_to_bool : Nat → Bool
 | 0 => false
 | _ => true
@@ -269,14 +271,13 @@ With this element conversion function we can now define
 our list conversion function. Given an empty list of Nat 
 we'll return an empty list of Bool. Otherwise, we have a
 bit (Nat) at the head of a non-empty list and t as the rest.
-In this case we'll return a new list with *bit_to_bool h*,
-a Boolean, at the head and the conversion of the rest of 
-the list as its tail. 
 -/
 
 def bit_list_to_bool_list : List Nat → List Bool
 | [] => []
 | h::t => (bit_to_bool h) :: (bit_list_to_bool_list t)
+
+#eval bit_list_to_bool_list [0, 0, 0, 1, 0, 1]
 
 /-!
 Given row and columns return list of Bools
@@ -284,6 +285,8 @@ Given row and columns return list of Bools
 
 def mk_row_bools : (row : Nat) → (cols : Nat) → List Bool
 | r, c => bit_list_to_bool_list (mk_bit_row r c)
+
+#eval mk_row_bools 5 6  -- expect [false, false, false, true, false, true]
 
 /-!
 #### List Bool to (var → Bool) Interpretation
@@ -354,4 +357,3 @@ def row_6_vars_3 := bit_list_to_bool_list (mk_bit_row 6 3)  -- expect [t,t,f]
 #eval (mk_interp_r_c 6 3) (var.mk 0)  -- expect true  (1)
 #eval (mk_interp_r_c 6 3) (var.mk 1)  -- expect true  (1)
 #eval (mk_interp_r_c 6 3) (var.mk 2)  -- expect false (0)
-
