0f4de40cf6f59e56a9103e517acf4ffc5f74c71d
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-10-11 16:24:49 +0000

    .

--- Instructor/Cuts/sat_solver_stuff.lean
+++ Instructor/Cuts/sat_solver_stuff.lean
@@ -65,3 +65,49 @@ warning, and we have a final algorithm, good enough
 for government work, as they say.
 -/
 
+
+
+
+
+/-!
+The rightmost digit of a binary expansion of a number, n,
+is 0 if n is even and 1 if n is odd. In other words, the
+rightmost binary digit is n%2. To get the next digit, you
+use integer division to divide n by 2, and repeat. 
+
+Here's a first version. If the input is 0 it returns the
+list of binary digits, [0]. Otherwise it returns a list 
+of all the digits to the left of the last digit, whcih is
+computed recursively, with the singlton list containing
+the correct rightmost digit appended at the end.
+
+As an example, suppose we're given a row index of 5.
+We want its binary expansion as a list of bits, which 
+is [1,0,1]. That's *1*2^2 + 0*2^1 + 1*2^0*, which in
+turn is *4 + 0 * 1*, which is 5. 
+
+The basic idea then is as follows. If the input, n, is 
+0, we'll return [0] (the list containing just zero). If
+the the input is 1, we'll return [1]. Otherwise n is at
+least 2, so we can write it as n = n' + 2. We thus have
+three cases to consider, and have already give solutions
+for the first two.
+
+So now suppose *n = n' + 2* for some *n'*. The rightmost 
+bit is (n%2)*. Once we have this bit in hand, we want to 
+eliminate it, shift all the remaining bits one place to 
+the right and repeat. To throw away the rightmost bit of 
+*n*, we just divide it by 2.
+
+So now we have our algorithm. If n is a base case, output 
+the corresponding singleton list of binary digits. In any
+other case, we output the list obtained by appending two
+lists: (1) the recursively computed list of digits to the
+left of the rightmost bit, and (2) the list containing just
+the rightmost bit. 
+
+To make our code even clearer, we'll *reify* the abstract
+operations of extracting the rightmost bit, and shifting
+all remaining bits to the right.
+-/
+
