6d96eddb7f480b35370c97b4b5542c1f55588027
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-11-04 21:44:38 +0000

    updates to lecture 17

--- Instructor/Lectures/lecture_17.lean
+++ Instructor/Lectures/lecture_17.lean
@@ -88,96 +88,120 @@ example (A B : Prop) : ¬A ∨ ¬B → ¬(A ∧ B)
   λ ⟨ _, b ⟩ => nb b
 
 /-!
+### Not All Classical Reasoning is Constructively Valid
+
+Now here's a proposition that seems that it ought to be
+true. It is *classically* true. You can check it using our
+validity checker, with A and B as propositional variables.
+But we struggled to prove it *constructively* in class, and
+there's a good reason for that: it's impossible to construct
+a proof of either ¬A or ¬B from a proof of ¬(A ∧ B). This one
+of the four DeMorgan's laws is clasically valid but it is
+*not valid* in constructive logic. No wonder we had trouble
+proving it!
+-/
+
+example (A B : Prop) :  ¬(A ∧ B) → (¬A ∨ ¬B)
+| nab => _    -- nowhere to go, we're stuck
+
+/-!
+So are we stuck in Lean with a logic that's actually weaker
+that propositional or first-order predicate logic--capable of
+proving fewer theorems? What constructive lacks relative to
+classical logics is *the law of the excluded middle* an axiom.
+
+-/
+
+/-!
 ## The Law of the Excluded Middle as an Independent Axiom
 -/
 
 /-!
-Unlike in classical (e.g., propositional) logic, where
-the proposition, X ∨ ¬X is *valid*, it's is not valid in
-the *constructive* logic of the Lean prover. Before you
-go on, remind yourself of why it's valid in propositional
-logic. Answer: it's by case analysis on possible values of
-X.
-
-In classical (propositional, first-order predicate) logic,
-given any *proposition*, P, P is either true or it is false,
-and in either case P ∨ ¬P is true, and so it's also *valid*.
-This principle of logical reasoning is called *the law of
-the excluded middle*.
-
-In the constructive predicate logic of Lean, however, X ∨ ¬X
-is not valid. Why? There are only two ways to construct a proof
-of a *disjunction*: either giving a proof of *X* or giving a
-proof of *¬X*. We don't have either to start with, so there's
-no way to proceed.
+In classical logics, a proposition is either true or false. In
+constructive logic, a proposition, P, is true if there's a proof
+of it, or it's false if there's a proof of ¬P, but its validity
+is indeterminate if there isn't a proof either way. Consequently,
+the proposition, P ∨ ¬P, is not necessarily valid. We might not
+have either a proof of P or of ¬P, in which case we can't prove
+the disjunction.
 -/
+
 example : X ∨ ¬X := _   -- no proof; not *constructively valid*
 
 /-!
-We can however, add the law of the excluded middle,
-as a new *axiom*, to the constructive logic of Lean, as
-follows.
+A key difference between constructive and classical logic is that
+the latter takes the *law of the excluded middle (em)* as an axiom.
+It lets you to assume that for any proposition, P, *P ∨ ¬P* is true,
+and you can therefore do a case analysis, with one case where P is
+true (and in Lean has a proof) and one case where ¬P is true (and
+has a proof).
+
+Taking the law of the excluded middle turns constructive logic back
+into a classical logic. There is no longer a middle, indeterminate,
+state to consider. For each proposition, there are only two possible
+states: that it's true or that its negation is true. Acccepting this
+assumption as an axiom makes it possible once again to prove that
+our variant of DeMorgan's law is valid. So let's dive in and see how
+this works.
+-/
+
+/-!
+First, here's the formal statement of the law of the excluded middle.
+It says that if you give and proposition, P, it'll return a proof/value
+of P ∨ ¬P.
 -/
 axiom em (P : Prop): P ∨ ¬ P
+
 /-!
 The axiom keywork instructs Lean to accept a definition
 without a proof, or implementation. What we have here is
 the definition of *em* (for *excluded middle*) as being a
 kind of *proof generator:* you feed it any proposition,
-*P*, and it returns a *proof* of the further proposition,
-*P ∨ ¬P*.
+*P*, and it returns a *proof* of the proposition, *P ∨ ¬P*.
 
-Once you have such a proof, you can do *case analysis*
-on it: in the first case, you'll have a proof of *P*,
-and in the second case, you'll have a proof of ¬P.
-One has thus excluded the possibility of the "middle"
-case, where one doesn't have a proof either way.
+Now here's the crucial trick: Once you have such a proof,
+you can do *case analysis* on it. In the first case (Or.inl p),
+you'll have a proof *p : P*. In the second case, you'll have a
+proof, np, of ¬P. One has thus excluded the possibility of a
+"middle" case, where one doesn't have a proof either way.
 -/
 
 /-!
-Now proving X ∨ ¬X is trivial. It follows by a simple
-application of em to X.
+Now, of course, proving a proposition, X ∨ ¬X, is trivial,
+no matter what proposition X represents. The validity of
+*X ∨ ¬X* follows from a simple application of em to X.
+-/
 
-Finally, we've see that if given any proposition, P, and
+
+def P : Prop := _
+#check em P
+
+/-!
+So we've now seen that if given any proposition, P, and
 the axiom of the excluded middle, you can always obtain
-a proof of P ∨ ¬P for free. The trick is then to do case
-analysis on that proof, with just two cases: Either P is
-true, and you can assume you have a proof, p, of P; or P
-is false, and you can assume you have a proof, np, of ¬P.
-The cases will be either *Or.inl p*, where (p : P), or
-*Or.inr np*, where (np : ¬P). The "middle" case, where we
-don't have a proof either way, is excluded.
+a proof of P ∨ ¬P for free. The trick now, again, is to do
+case analysis on that proof, There are just two cases:
+Or.inl with a proof (p : P) or Or.inr with a proof(np : ¬P).
+The "middle" case, where we don't don't  a proof either
+way, no longer needs to be considered.
 -/
 example : X ∨ ¬X := em X
 
 /-!
-### One of DeMorgan's Laws is *Not* Valid in Lean
-
-##
-
-Now here's a proposition that seems that it ought to be
-true. It is *classically* true. You can check it using our
-validity checker, with A and B as propositional variables.
-But we struggled to prove it *constructively* (using Lean).
-From a proof of ¬(A ∧ B) (A ∧ B → False), it's impossible
-to derive a proof of ¬A or a proof of ¬B, and we'd need at
-least one of those proofs to prove ¬A ∨ ¬B. This proposition
-is *not valid* in the constructive logic of Lean. No wonder
-we had trouble proving it.
-
 ### Use *em* For Classical Reasoning
 
 Accepting the law of the excluded middle as an axiom puts
 us back in classical reasoning space. Using it, you can get
-yourself proofs of both A ∨ ¬A and B ∨ ¬B, and now it's just
-a matter of showing that the proposition is true in each of
-the four resulting cases.
+yourself proofs of both A ∨ ¬A and B ∨ ¬B, and now, to prove
+the validity of the DeMorgan's law variant that had us hung up,
+it's just a matter of showing that the proposition is true in each
+of the four resulting cases.
 
 HOMEWORK: Complete this proof.
 -/
 
 example (A B : Prop) : ¬(A ∧ B) -> ¬A ∨ ¬B :=
 λ nab =>
-let aornota := em A
-let bornotb := em B
+let proof_of_aornota := em A
+let proof_of_bornotb := em B
 _
