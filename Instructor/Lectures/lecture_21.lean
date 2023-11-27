76b485d79d44b3594d4e5d3d51e6f9d1cdcd3105
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-11-27 03:21:52 +0000

    Updates from laptop.

--- Instructor/Lectures/lecture_21.lean
+++ Instructor/Lectures/lecture_21.lean
@@ -6,6 +6,10 @@ Predicates
 def ev := λ n : Nat => n % 2 = 0
 def small := λ n : Nat => n = 0 ∨ n = 1 ∨ n = 2 ∨ n = 3 ∨ n = 4
 
+#reduce small 1
+
+example : small 1 := Or.inr (Or.inl rfl)
+
 /-!
 Sets, defined by (and equivalent to membership) predicates
 -/
@@ -25,33 +29,20 @@ Membership proofs
 -/
 example : small 3 := Or.inr (Or.inr ( Or.inr (Or.inl rfl)))
 example : small_set 3 := Or.inr (Or.inr ( Or.inr (Or.inl rfl)))
-example : 3 ∈ small_set := Or.inr (Or.inr ( Or.inr (Or.inl rfl)))
+example : 3 ∈ small_set := (Or.inr (Or.inr (Or.inr (Or.inl rfl))))
 
-example : 7 ∉ small_set := λ h => nomatch h
 
 /-!
 Intersection
 
-If S and T are sets of natural numbers with membership
-predicates s : Nat → Prop and t : Nat → Prop, then S ∩ T
-is the set of values that satisfy both s and t, and is
-said to be the intersection of the two sets: the set of
-elements that are in both sets.
 -/
+def even_and_small (n : Nat) : Prop := ev n ∧ small n
+def even_and_small_set : Set Nat := { n | even_and_small n}
+def even_and_small_set' : Set Nat := { n | ev n ∧ small n}
 
-def even_and_small := λ (n : Nat) => ev n ∧ small n
-def even_and_small_set := { n : Nat | n ∈ ev_set ∧ n ∈ small_set}
-#reduce even_and_small 3
-#reduce 3 ∈ even_and_small_set
+example : 2 ∈ even_and_small_set := ⟨rfl , Or.inr (Or.inr (Or.inl rfl))⟩
 
-example : 3 ∉ even_and_small_set :=
-  λ h => nomatch h
 
-example : 4 ∈ even_and_small_set :=
-  ⟨ rfl, Or.inr (Or.inr ( Or.inr (Or.inr rfl))) ⟩
-
-example : 4 ∈ ev_set ∩ small_set :=
-  ⟨ rfl, Or.inr (Or.inr ( Or.inr (Or.inr rfl))) ⟩
 
 /-!
 Union
