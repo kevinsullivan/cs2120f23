c7bda4d517891f782d195b141b473c13e5cfa2ff
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-11-30 20:27:52 +0000

    IMPORT RELATIONS

--- Instructor/Lectures/lecture_21.lean
+++ Instructor/Lectures/lecture_21.lean
@@ -1,7 +1,8 @@
 import Mathlib.Data.Set.Basic
+import Mathlib.Logic.Relation
+
 
 /-!
-
 # Set Theory
 
 A set is intuitively understood as a collection of objects.
@@ -596,55 +597,11 @@ variable (s t : Set Nat)
 | Set Theory  | Set Theory Definitions    | Predicate Logic                   |
 |-------------|---------------------------|-----------------------------------|
 | set α       | axioms of set theory      | predicate (α → Prop in Lean)      |
-| s ∩ t       | { a | a ∈ s ∧ a ∈ t }     | λ a => s a ∧ t a                  |
-| s ∪ t       | { a | a ∈ s ∨ a ∈ t }     | λ a => s a ∨ t a                  |
-| sᶜ          | { a | a ∉ s }             | λ a => s a → False                |
-| s \ t       | { a | a ∈ s ∧ a ∉ t }     | λ a => s a ∧ (t a → False)        |
+| s ∩ t       | { a \| a ∈ s ∧ a ∈ t }     | λ a => s a ∧ t a                  |
+| s ∪ t       | { a \| a ∈ s ∨ a ∈ t }     | λ a => s a ∨ t a                  |
+| sᶜ          | { a \| a ∉ s }             | λ a => s a → False                |
+| s \ t       | { a \| a ∈ s ∧ a ∉ t }     | λ a => s a ∧ (t a → False)        |
 | s ⊆ t       | ∀ a, a ∈ s → a ∈ t  ...   | λ a => s a → t a ...              |
 | s ⊊ t       | ... ∧ ∃ w, w ∈ t ∧ w ∉ s  | ... ∧ ∃ w, (t w) ∧ (s w → False)  |
-| 𝒫 s         | { b : Set s | b ⊆ univ }  | λ b => b ⊆ univ                   |
--/
-
-/-!
-## Exercises
-
-### Transitivity of Subset Relation
-
-Suppose we have sets, s1, s2, and s3, with s1 ⊆ s2 and s2 ⊆ s3.
-Does it necessarily follow that s1 ⊆ s3? In other words, is it
-the case that s1 ⊆ s2 → s2 ⊆ s3 → s1 ⊆ s3? First think about in
-concrete terms. For example, suppose a bag of pearls is inside a
-bag of rice, and that in turn is inside a bag of straw. Are the
-pearls inside the bag of straw? Yeah, really they are. Formalize
-and prove it in Lean. Hint State the proposition in the language
-of set theory then construct a proof of its reduction to logical
-definitions.
-
-### Distributivity of Intersection Over Union
--/
-
-#check ∀ (α : Type) (r s t : Set α), (r ∩ (s ∪ t)) = ((r ∩ s) ∪ (r ∩ t))
-
-
-#check propext
-
-example (α : Type) (r s t : Set α) : (r ∩ (s ∪ t)) = ((r ∩ s) ∪ (r ∩ t)) := by
-    apply Set.ext   -- replaces = with logical ↔
-
-    intro x
-    apply Iff.intro
-
-    -- forward
-    intro h
-    cases h with
-    | intro hl hr =>
-      _
-
-
-
-
-/-!
-### DeMorgan's Laws In Set Theory
-
-Suppose s, t, and u are sets. Is it the case that
+| 𝒫 s         | { b : Set s \| b ⊆ univ }  | λ b => b ⊆ univ                   |
 -/
