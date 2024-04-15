f53627cce170863adb7125a80a76ca81667ca850
Kevin Sullivan <sullivan.kevinj@gmail.com>
2024-04-15 14:42:14 +0000

    Sets/Relations

--- Instructor/Lectures/grad/sets_relations.lean
+++ Instructor/Lectures/grad/sets_relations.lean
@@ -1,8 +1,54 @@
-import Mathlib.
+import Mathlib.Logic.Relation
+import Mathlib.Logic.Function.Basic
+
+/-!
+binary relation on a type, α 
+- reflexive
+- symmetric
+- transitive
+- equivalence (Core.lean)
+- asymmetric
+- antisymmetric
+- closures
+- inverse
+
+- empty relation
+- complete relation
+- subrelation
+
+binary relation from α → β, etc 
+- compose
+- join
+
+inverse image
+-/
 
 /-
-- order relations
-- partial order
-- total order
-- poset
+order relations
+- partial order: reflexive, antisymmetric, transitive
+- poset: a set α along with a partial order on α
+- total order: partial order ∧ ∀ a b, a < b ∨ b < a
 -/
+
+/-
+functions
+- a single-valued relation is a function
+
+- domain of definition
+- codomain
+- domain
+- range
+- partial
+- total 
+
+- identity function -- See Mathlib.Logic.Function.Basic
+
+- one-to-one (vs many-to-one, injective)
+- onto (surjective)
+- bijective
+
+- composition
+- inverse
+- etc
+-/
+
